6ae426d80d6764e418c0114f6b82f7377e4f2153
luming1617 <luming1617@qq.com>
2017-08-11 18:29:01 +0800

    合并整理

--- src/main/java/net/TCPSocketDemo1.java
+++ src/main/java/net/TCPSocketDemo1.java
@@ -1,38 +1,127 @@
 package net;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /*
-* 演示一：创建使用TCP协议的客户端.
+* TCP Socket 演示一：文件上传.
 */
 public class TCPSocketDemo1 {
 
-    public static void main(String[] args) throws IOException {
-        client();
-    }
+    public static void main(String[] args) throws Exception {
 
-    static void client() throws IOException {
+        // 创建服务器端对象
+        ServerSocket ss = new ServerSocket(9999);
 
-        System.out.println("客户端 启动.......");
-//        创建客户端socket对象。明确服务端地址和端口。
-        Socket s = new Socket("127.0.0.1", 10004);
-//        发送数据，通过socket输出流完成。
-        OutputStream out = s.getOutputStream();
-        out.write("服务端，我来了".getBytes());
-//        读取服务端返回的数据，通过socket输入流
-        InputStream in = s.getInputStream();
-        byte[] buf = new byte[1024];
-        int len = in.read(buf);
-        String text = new String(buf, 0, len);
-        System.out.println(text);
-//        关闭资源。
-        s.close();
+        while (true) {
+            // 获取客户端对象
+            Socket s = ss.accept();
+            // 一个客户端启用一个线程
+            new Thread(new TCPServer(s)).start();
+        }
     }
 }
-//总结：
+
+// 服务端
+class TCPServer implements Runnable {
+    Socket s = null;
+
+    public TCPServer(Socket s) {
+        this.s = s;
+    }
+
+    public void run() {
+        try {
+            // 获取连接的客户的IP
+            String ip = s.getInetAddress().getHostAddress();
+            // 从客户端获取输入流
+            InputStream in = s.getInputStream();
+            // 创建输出的路径对象
+            File dir = new File("./log/");
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            int count = 1;
+            // 创建输出的文件对象
+            // TODO 获得文件信息
+            File filePic = new File(dir, ip + "-" + count + ".png");
+            while (filePic.exists()) {
+                count++;
+                filePic = new File(dir, ip + "-" + count + ".png");
+            }
+            // 创建文件输出流对象
+            FileOutputStream fos = new FileOutputStream(filePic);
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buf)) != -1) {
+                fos.write(buf, 0, len);
+            }
+            // 设置返回信息
+            OutputStream out = s.getOutputStream();
+            out.write("上传成功".getBytes());
+            fos.close();
+            s.close();
+        } catch (IOException e) {
+        }
+    }
+}
+
+// 客户端
+class TCPClient {
+
+    // 服务端地址
+    static String ip = "127.0.0.1";
+
+    // 服务端端口
+    static int port = 9999;
+
+    public static void main(String[] args) throws Exception {
+        send("/Users/liuluming/Documents/Java IO.png");
+    }
+
+    static void send(String filePath) throws IOException {
+        System.out.println("开始上传文件......");
+        // 创建socket。
+        Socket s = new Socket(ip, port);
+        // 读取源文件。
+        File picFile = new File(filePath);
+        if (!picFile.exists()) {
+            throw new IOException("文件不存在");
+        }
+        FileInputStream fis = new FileInputStream(picFile);
+
+        // 获得socket输出流。
+        // TODO 封装文件信息（SequenceInputStream）
+        OutputStream out = s.getOutputStream();
+        byte[] buf = new byte[1024];
+        int len = 0;
+        // 让输出流从文件读取内容弄。
+        while ((len = fis.read(buf)) != -1) {
+            out.write(buf, 0, len);
+        }
+        // 告诉服务器端图片数据发送完毕，不要等着读了。
+        s.shutdownOutput();
+        // 获取返回信息。
+        InputStream in = s.getInputStream();
+        byte[] bufIn = new byte[1024];
+        int lenIn = in.read(bufIn);
+        System.out.println(new String(bufIn, 0, lenIn));
+        // 关闭。
+        fis.close();
+        System.out.println("结束上传文件......");
+        s.close();
+    }
+
+}
+
+//【客户端】总结：
 // 1、创建TCP协议的【客户端】：使用Socket带参构造方法（指定服务端地址和端口）。
 // 2、给服务端发送数据：先使用客户端对象的getOutputStream()方法获得输出流，然后写出。
 // 3、获取服务端返回的数据：先使用客户端对象的getInputStream()方法输入流，然后读入。
+
+//【服务端】总结：
+// 1、创建TCP协议的【服务端】：使用ServerSocket带参构造方法（指定端口）。
+// 2、获取接收到的客户端对象（Socket）：使用ServerSocket的accept方法。
+// 3、获取客户端发来的数据：先使用客户端对象的getInputStream()方法获得输入流，然后读入。
+// 4、给客户端返回数据：先使用客户端对象的getOutputStream()方法获得输出流，然后写出。
--- src/main/java/net/UDPSocketDemo1.java
+++ src/main/java/net/UDPSocketDemo1.java
@@ -1,45 +1,126 @@
 package net;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /*
- * 演示二：创建使用UDP协议的接收端
+ * UDP Socket（DatagramSocket）演示一：聊天室
  */
 public class UDPSocketDemo1 {
 
-    public static void main(String[] args) throws IOException {
-        receive();
-    }
+    public static void main(String[] args) throws Exception {
 
+        //创建【发送端】
+        UDPSender s = new UDPSender("127.0.0.1",1234);
 
-    static void receive() throws IOException {
-        //创建接收端对象，由于发送断是给10000端口上发送，创建接收端的时候，端口需要和发送端一致，否则收不到数据
-        DatagramSocket socket = new DatagramSocket(10000);
-        while (true) {
-            //创建数组对象，用于接收数据
-            byte[] buf = new byte[1024];
-            //创建用于接收发送端发送过来的数据包
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
-            //接收数据
-            socket.receive(packet);
-            //通过数据包对象获取发送端发送来的数据和发送端的信息
-            String ip = packet.getAddress().getHostAddress();
-            //获取发送端的端口号
-            int port = packet.getPort();
-            //获取发送的数据
-            String value = new String(packet.getData(), 0, packet.getLength());
-            //打印接收的数据
-            System.out.println(ip + ":" + port + ":" + value);
-        }
-        //关闭接收端。如果不断的获取客户端，不用关闭服务端。
-//        socket.close();
+        //创建【接收端】
+        UDPReceiver r = new UDPReceiver(1234);
+
+        //创建线程并开启
+        new Thread(r).start();
+        Thread.sleep(1000);
+        new Thread(s).start();
     }
 }
 
-// 总结：
+// 发送端
+class UDPSender implements Runnable {
+
+    // 服务端地址
+    String ip;
+
+    // 服务端端口
+    int port;
+
+    // 发送端Socket
+    DatagramSocket socket = null;
+
+    public UDPSender(String ip, int port) throws IOException {
+        this.ip = ip;
+        this.port = port;
+        // 创建发送端Socket对象，此时没有指定端口。
+        socket =new DatagramSocket();
+    }
+
+    public void run() {
+        System.out.println("发送端已启动，请输入：");
+        // 从键盘输入内容
+        BufferedReader buffr = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String line = null;
+            while ((line = buffr.readLine()) != null) {
+                byte[] buf = line.getBytes();
+                //指定数据发送给具体的接收端对象（指定ip和port）
+                DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName(ip), port);
+                // 发送数据
+                socket.send(dp);
+                // 如果输入的字符是over，结束发送。
+                if ("886".equals(line)) {
+                    socket.close();
+                }
+            }
+        } catch (IOException e) {
+        }
+    }
+}
+
+// 接收端
+class UDPReceiver implements Runnable {
+
+    // 接收端端口
+    int port;
+
+    // 接收端Socket
+    DatagramSocket socket = null;
+
+    public UDPReceiver(int port) throws IOException{
+        this.port=port;
+        // 创建接收端对象，并指定端口。
+        this.socket = new DatagramSocket(port);
+    }
+
+    public void run() {
+        System.out.println("接收端已启动");
+        while (true) {
+            try {
+                //创建数组对象，用于接收数据
+                byte[] buf = new byte[1024];
+                //创建用于接收发送端数据的包
+                DatagramPacket dp = new DatagramPacket(buf, buf.length);
+                //接收数据
+                socket.receive(dp);
+                //通过【数据包对象】获取发送端的相关信息
+                // 获取发送端ip。
+                String ip = dp.getAddress().getHostAddress();
+                //获取发送端端口号
+                int port = dp.getPort();
+                //获取发送的数据
+                String value = new String(dp.getData(), 0, dp.getLength());
+                System.out.println("来自"+ip + ":" + port + "的消息:" + value);
+
+                if (value.equals("886")) {
+                    System.out.println(ip + "离开了聊天室");
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+}
+
+// 【接收端】总结：
 // 1、创建UDP的【接收端】：使用DatagramSocket的【带参构造】方法。
 // 2、创建用于接收数据的包：使用DatagramPacket的带参构造方法。
 // 3、接收数据：使用DatagramSocket类的receive(DatagramPacket)方法。
+
+// 【发送端】总结
+// 1、创建UDP的【发送端】：使用DatagramSocket的【无参构造】方法。
+// 2、创建用于发送数据的包：使用DatagramPacket的带参构造方法（指定服务端地址和端口号）。
+// 3、发送数据：使用DatagramSocket类的send(DatagramPacket)方法。
+
+// TCP & UDP 区别：https://zhuanlan.zhihu.com/p/26649540
 
